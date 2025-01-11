6c59a44384b60c152327fdd4a028df0a3f711b5c
Eli Belash <elibelash@gmail.com>
2025-01-11 16:50:57 +0200

    test: hotfix

--- FontRegister.UnitTests/WindowsUserFontInstallerTests.cs
+++ FontRegister.UnitTests/WindowsUserFontInstallerTests.cs
@@ -51,12 +51,15 @@ namespace FontRegister.UnitTests
             if (!Path.HasExtension(normalizedName))
                 normalizedName += ".ttf";
 
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+            
             // Act
             var result = _installer.UninstallFont(normalizedName);
 
             // Assert
             Assert.That(result, Is.False, "Should return false for non-existent font");
-            Assert.That(Console.Out.ToString(), Does.Contain("Font not found anywhere"), "Expected console output not found");
+            Assert.That(consoleOutput.ToString(), Does.Contain("Font not found anywhere"), "Expected console output not found");
         }
 
         [Test]
